ec0bc1fa2f9d586e052b7bba3bf1af2c80132c57
msowjanya <msowjanya2014@gmail.com>
2018-09-18 14:06:09 +0530

    jenkins modified

--- javaProject/src/com/sample/project/Sample.java
+++ javaProject/src/com/sample/project/Sample.java
@@ -2,7 +2,7 @@ package com.sample.project;
 
 public class Sample {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("hello jenkins   "+i);
 			
 		}
