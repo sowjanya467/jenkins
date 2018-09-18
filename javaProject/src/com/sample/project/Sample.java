cdc49e91ecfeb300b810c2a7a6f8cb7f968287ba
msowjanya <msowjanya2014@gmail.com>
2018-09-18 13:05:10 +0530

    jenkins sample

---
+++ javaProject/src/com/sample/project/Sample.java
@@ -0,0 +1,13 @@
+package com.sample.project;
+
+public class Sample {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello jenkins   "+i);
+			
+		}
+		
+		
+	}
+
+}
